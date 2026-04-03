704a859952ad11f31f24831c23bc76cb5cad52f6
ncvangilse <ncvangilse@users.noreply.github.com>
2026-04-03 07:59:15 +0000

    deploy: 0617eef8c15305dd90a4fb8499bbead27ddf317b

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.04.03-95-claude-fix-dividers-portrait-mode-5IPMH-5';
+const CACHE_NAME = 'vejr-2026.04.03-96-claude-fix-dividers-portrait-mode-5IPMH-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
