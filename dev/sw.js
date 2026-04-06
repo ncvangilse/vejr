c66e72f1b6cb6856f6b5024478cfee0de88b54d3
ncvangilse <ncvangilse@users.noreply.github.com>
2026-04-06 06:39:49 +0000

    deploy: ee8b51864641cedc39c3b81230eb1ae6cd6030a4

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.04.06-117-claude-robust-sea-bearing-algo-7qlrA-5';
+const CACHE_NAME = 'vejr-2026.04.06-118-claude-robust-sea-bearing-algo-7qlrA-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
