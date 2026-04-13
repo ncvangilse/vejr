76a2c51ed4821ed20413c264fb8ac26a76dc2882
ncvangilse <ncvangilse@users.noreply.github.com>
2026-04-13 20:01:16 +0000

    deploy: 87d4b7c29d56374c77268b710f8dabd82bf58c34

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.04.13-30-nchvg-dmi_data-5';
+const CACHE_NAME = 'vejr-2026.04.13-31-nchvg-improved-dmi-popups-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
