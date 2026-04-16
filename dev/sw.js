9b24b807541ea84ea1aa1b4be0abf35bae1d4dea
ncvangilse <ncvangilse@users.noreply.github.com>
2026-04-16 05:47:44 +0000

    deploy: c0d1257d23e27cdd567eb7ca39dd7bdce6ecfa57

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.04.16-87-claude-add-credits-screen-AXXQa-5';
+const CACHE_NAME = 'vejr-2026.04.16-88-claude-add-credits-screen-AXXQa-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
