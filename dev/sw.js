e47dc51ddffa29a54ba404791614d8bfb220cfd6
ncvangilse <ncvangilse@users.noreply.github.com>
2026-04-17 11:57:05 +0000

    deploy: 3369092557852b895ff5051197dade3ec8b58efa

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.04.17-95-nchvg-json_refactor-5';
+const CACHE_NAME = 'vejr-2026.04.17-96-nchvg-json_refactor-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
