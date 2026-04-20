537a33aa545f5278d0af83407462a0a2f15cc7f2
ncvangilse <ncvangilse@users.noreply.github.com>
2026-04-20 09:31:49 +0000

    deploy: e978146e5e5d160619c2f32a354e825e1330a69c

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.04.20-103-nchvg-clickable_points_2-5';
+const CACHE_NAME = 'vejr-2026.04.20-104-claude-fix-kite-weather-indicator-NIaW0-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
