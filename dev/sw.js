b6e7549924bb1779f33b064488a5a7c6438c189b
ncvangilse <ncvangilse@users.noreply.github.com>
2026-04-22 07:56:12 +0000

    deploy: 36906ecb8c866e8a4ba0e3a17c58414020b06314

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.04.22-152-claude-fix-ios-icon-cache-QsJRg-5';
+const CACHE_NAME = 'vejr-2026.04.22-153-claude-fix-ios-icon-cache-QsJRg-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
