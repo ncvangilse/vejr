b64efc59e769112d07d886f8b8e6382168074692
ncvangilse <ncvangilse@users.noreply.github.com>
2026-04-25 14:17:52 +0000

    deploy: 984b43698d9d13e4ad92ce2fc0063572d3d4c399

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.04.25-230-claude-issue-90-E8pof-5';
+const CACHE_NAME = 'vejr-2026.04.25-231-claude-fix-issue-114-4jXmB-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
