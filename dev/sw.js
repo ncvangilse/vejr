b09703a9de4f7b70e2255974baa0bde7e3de7b22
ncvangilse <ncvangilse@users.noreply.github.com>
2026-05-05 11:33:35 +0000

    deploy: 509982bf1fff079d412eb43c1498998139512bf0

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vejr-2026.05.05-250-claude-fix-dark-mode-colors-xiU1O-5';
+const CACHE_NAME = 'vejr-2026.05.05-251-claude-fix-dark-mode-colors-xiU1O-5';
 
 // Only cache truly static assets — never the HTML or SW itself
 const ASSETS = [
